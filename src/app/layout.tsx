2de5d68935eca1e0a0df980ed3fde96fe01ba871
Xuefeng Zhu <frankzhu94@gmail.com>
2026-03-28 13:10:38 -0700

    feat: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next';
+import './globals.css';
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+export const metadata: Metadata = {
+  title: 'adaptive-learning',
+  description: 'Created with Next.js',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={cn("font-sans", geist.variable)}>
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
